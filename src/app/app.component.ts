da1da0a6b590b971076c77114db3cc7efcbec885
Vitor Rodrigues <vitor.rodriguess3112@gmail.com>
2025-05-20 04:30:49 -0300

    configurado prime ng + tela 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule, NgForm } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { ButtonModule } from 'primeng/button';
 
+//organizando imports de componentes do prime ng, botão inputs etc
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [
+    RouterOutlet,
+    FloatLabelModule,
+    InputTextModule,
+    FormsModule,
+    DropdownModule,
+    ButtonModule
+  ]
 })
+
 export class AppComponent {
-  title = 'desafio-frontend';
+  //variáveis das forms
+  nome: string = '';
+  sobrenome: string = '';
+  generoSelecionado: string = '';
+
+  //lista generos
+  opcoesGeneros = [
+    { label: 'Homem', value: 'Homem' },
+    { label: 'Mulher', value: 'Mulher' },
+    { label: 'Prefiro não responder', value: 'Prefiro não responder' }
+  ];
+
+  onSubmit(form?: NgForm) {
+    // teste pra ver se tá pegando info
+    console.log('Formulário enviado:', {
+      nome: this.nome,
+      sobrenome: this.sobrenome,
+      generoSelecionado: this.generoSelecionado
+    });
+  }
 }
